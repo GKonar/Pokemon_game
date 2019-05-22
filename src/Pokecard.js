cd4671a99662176b8577b10fe23ea61d2759b58f
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2019-05-22 11:04:15 +0200

    Add HQ pictures of pokes

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import './Pokecard.css';
 
-const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon';
+// const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon';
+const POKE_HD_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
 
 class Pokecard extends Component {
+	addZero(num) {
+		if (num < 10) {
+			return '00' + num;
+		} else if (num >= 10 && num < 100) {
+			return '0' + num;
+		} else {
+			return '' + num;
+		}
+	}
+
 	render() {
-		let imgSrc = `${POKE_API}/${this.props.id}.png`;
+		let imgSrc = `${POKE_HD_API}/${this.addZero(this.props.id)}.png`;
 		return (
 			<div className="Pokecard">
 				<h2>{this.props.name}</h2>
